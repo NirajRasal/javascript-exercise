8878003c194ad523ed95f93538bb8e413f7904e0
NirajRasal <48594267+NirajRasal@users.noreply.github.com>
2020-06-17 19:30:50 +0530

    javascript exercise

---
+++ javascript-exercises/repeatString/repeatString.js
@@ -0,0 +1,15 @@
+const repeatString = function(word,times) {
+	var string  = "";
+	if (times < 0)
+	{
+		return "ERROR";
+	}
+	for (var i=0; i<times; i++)
+	{
+		string=string+word;
+	}
+	return string;
+
+}
+
+module.exports = repeatString
