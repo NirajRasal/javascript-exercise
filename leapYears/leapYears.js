2265bc71af1a1de4a33af5e207dc06feba9c35ff
NirajRasal <nirajrasal17@gmail.com>
2020-06-20 15:48:30 +0530

    solved leap-year exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
+if(year % 100 ==0)			//identify centuary year
+{
+	if(year %400==0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 
+}
+else if(year % 4 == 0)		//for non-centuary year
+	{
+	return true;
+	}
+else
+	{
+	return false;
+	}
 }
 
 module.exports = leapYears
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,6 @@
-const reverseString = function() {
+const reverseString = function(word) {
+var original_word=word;
+var split=original_word.split("");
 
 }
 
