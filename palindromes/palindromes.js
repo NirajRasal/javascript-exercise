d9930f0417116c1d9a8a14e5ae3bc3de04dd4bb1
NirajRasal <nirajrasal17@gmail.com>
2020-06-28 20:15:00 +0530

    solved palindrome exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function() {
+const palindromes = function(input) {
+var regex=/[^A-Za-z0–9]/g;  						//regex for puntuation
 
+var makelowercase=input.toLowerCase();				//to make all alphabets lowecase
+var removespace=makelowercase.trim();				//to remove spaces from string
+var removepuntuation=removespace.replace(regex,'');	//to remove puntuation from string
+
+var inputstring=removepuntuation;
+
+var newstring="";
+for(var i=inputstring.length-1;i>=0;i--)			//reverse string
+	{
+		newstring=newstring+inputstring[i];
+	}
+	
+	if(newstring == inputstring)					//compare string
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 module.exports = palindromes
