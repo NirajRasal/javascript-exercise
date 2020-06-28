47c7458637e89c45cd821a7d8ab9bb4a37d09b77
NirajRasal <nirajrasal17@gmail.com>
2020-06-28 19:30:09 +0530

    solved calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,69 @@
-function add () {
+function add (first_no,second_no) {
+	var result=first_no+second_no;
+	return result;
+}
+
+function subtract (first_no,second_no) {
+	var result=first_no-second_no
+	return result;
 	
 }
 
-function subtract () {
-	
+function sum(input){
+             
+ if (toString.call(input) !== "[object Array]")		//from w3resources.com
+    return false;
+      
+            var total =  0;
+            for(var i=0;i<input.length;i++)
+              {                  
+                if(isNaN(input[i])){
+                continue;
+                 }
+                  total += Number(input[i]);
+               }
+             return total;
+            }
+
+function multiply (input) {
+
+ if (toString.call(input) !== "[object Array]")		//from w3resources.com
+    return false;
+      
+            var total =  1;
+            for(var i=0;i<input.length;i++)
+              {                  
+                if(isNaN(input[i])){
+                continue;
+                 }
+                  total *= Number(input[i]);
+               }
+             return total;
+            }
+
+
+function power(base,power) {
+	var result=base;
+	for(var i=1; i<power; i++)
+	{
+		var result=result*base;
+		
+	}
+	return result;
 }
 
-function sum () {
-	
-}
-
-function multiply () {
-	
-}
-
-function power() {
-	
-}
-
-function factorial() {
-	
+function factorial(no) {
+	var result=no;
+	if(no == 0)
+	{
+		return 1;
+	}
+	var temp=no-1;
+	for(var i=temp;i>0;i--)
+	{
+		result=result*i;
+	}
+	return result;
 }
 
 module.exports = {
