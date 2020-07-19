7d82c510131e9e173fce1287a7d984801510fa0d
NirajRasal <nirajrasal17@gmail.com>
2020-07-19 20:34:27 +0530

    solved reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,7 +1,8 @@
 const reverseString = function(word) {
-var original_word=word;
-var split=original_word.split("");
-
+var split=word.split("");		//to convert string to array since we cannot reverse string directly
+var reverse=split.reverse();	//to reverse the array
+var join=reverse.join('');		//to convert back reversed array to string
+return join;
 }
 
 module.exports = reverseString
